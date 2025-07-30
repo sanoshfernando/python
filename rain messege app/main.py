c146f300adbf1b1b69a5b19d4d98887c77c7ec99
sanoshfernando <sanoshdamion1@gmail.com>
2025-07-31 00:13:50 +0100

    2

---
+++ rain messege app/main.py
@@ -0,0 +1,4 @@
+import requests
+
+response = requests.get(url="api.openweathermap.org/data/2.5/forecast?lat=55.005412&lon=-1.605277&appid=56ffb965f435437091020c845a88a7d4")
+56ffb965f435437091020c845a88a7d4
