4336d82d532d12987e3ec72b66c31c66da3f2e9e
sanoshfernando <sanoshdamion1@gmail.com>
2025-07-31 00:13:40 +0100

    1

--- quizzer app/quiz_brain.py
+++ quizzer app/quiz_brain.py
@@ -1,5 +1,6 @@
 import html
 
+
 class QuizBrain:
 
     def __init__(self, q_list):
@@ -17,14 +18,11 @@ class QuizBrain:
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
 
-
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
+            return False
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
