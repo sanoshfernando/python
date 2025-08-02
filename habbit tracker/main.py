7f9e1d9bcb7b27a84c91dca6bb6674ad5c7e83c6
sanoshfernando <sanoshdamion1@gmail.com>
2025-08-02 21:24:21 +0100

    3

---
+++ habbit tracker/main.py
@@ -0,0 +1,45 @@
+import requests
+from datetime import datetime
+
+pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "sanosh"
+TOKEN = "jnjsnvjsuiushnvlk"
+GRAPH_ID = "graph1"
+
+params = {
+    "token" : TOKEN,
+    "username" : USERNAME,
+    "agreeTermsOfService" : "yes",
+    "notMinor" : "yes"
+}
+
+user_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config ={
+    "id" : GRAPH_ID,
+    "name" : "Walking Graph",
+    "unit" : "Km",
+    "type" : "float",
+    "color" : "shibafu"
+}
+headers  =  {
+    "X-USER-TOKEN": TOKEN
+}
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+today_ = today.strftime("%Y%m%d")
+
+data = {
+    "date": "20250802",
+    "quantity": "10"
+}
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today_}"
+
+new_pixel_data = {
+    "quantity":"5"
+}
+
+response = requests.put(url=update_endpoint,json=new_pixel_data, headers = headers)
+print(response.text)
